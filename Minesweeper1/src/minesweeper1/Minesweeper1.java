3f398755b2bf29ecf267b1658961210973b08249
katelynpettit <katelynpettit@10.30.0.205>
2015-01-21 21:18:42 -0700

    Gal to Lit project

---
+++ Minesweeper1/src/minesweeper1/Minesweeper1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper1;
+
+/**
+ *
+ * @author katelynpettit
+ *///This is my fork!  Mine!!!!! jk
+public class Minesweeper1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
